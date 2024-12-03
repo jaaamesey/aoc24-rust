e0dab043021796eb316802ddaae434502865478d
James Karlsson <jameskarlsson1@gmail.com>
2024-12-03 20:17:55 +1100

    Part 2 done

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -14,4 +14,27 @@ pub fn part1() {
     dbg!(sum);
 }
 
-pub fn part2() {}
+pub fn part2() {
+    let input_str = include_str!("./real_input.txt").trim();
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)|(do)(\(\))|(don't)(\(\))").unwrap();
+    let mut enabled = true;
+    let sum = regex
+        .captures_iter(input_str)
+        .map(|c| {
+            let (_, [a, b]) = c.extract();
+            if a == "do" {
+                enabled = true;
+                return 0;
+            }
+            if a == "don't" {
+                enabled = false;
+                return 0;
+            }
+            if !enabled {
+                return 0;
+            }
+            return a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap();
+        })
+        .sum::<i32>();
+    dbg!(sum);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod day03;
 
 fn main() {
     let start = std::time::Instant::now();
-    day03::part1();
+    //   day03::part1();
     day03::part2();
     println!("Finished in {:?}", start.elapsed());
 }
