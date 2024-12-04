52c6e3469da6d3770b80f3890d6efd8d9da7dd77
James Karlsson <jameskarlsson1@gmail.com>
2024-12-04 19:00:25 +1100

    Part 2

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -41,4 +41,48 @@ pub fn part1() {
     dbg!(num_instances);
 }
 
-pub fn part2() {}
+pub fn part2() {
+    let board = include_str!("./real_input.txt")
+        .lines()
+        .map(|str| str.chars().collect())
+        .collect::<Vec<Vec<char>>>();
+    let directions: [(isize, isize); 4] = [(-1, -1), (-1, 1), (1, -1), (1, 1)];
+    let mut diagonal_centers = board
+        .iter()
+        .map(|row| row.iter().map(|_| 0).collect())
+        .collect::<Vec<Vec<usize>>>();
+    for (y, row) in board.iter().enumerate() {
+        for (x, c) in row.iter().enumerate() {
+            if *c != 'M' {
+                continue;
+            }
+            for (dy, dx) in directions {
+                let mut diagonal_center: Option<(usize, usize)> = None;
+                for (i, target_char) in "MAS".chars().enumerate() {
+                    let by = y.checked_add_signed(dy * (i as isize));
+                    let bx = x.checked_add_signed(dx * (i as isize));
+                    if by.is_none()
+                        || bx.is_none()
+                        || by.unwrap() > board.len() - 1
+                        || bx.unwrap() > row.len() - 1
+                        || board[by.unwrap()][bx.unwrap()] != target_char
+                    {
+                        diagonal_center = None;
+                        break;
+                    }
+                    if i == 1 {
+                        diagonal_center = Some((by.unwrap(), bx.unwrap()))
+                    };
+                }
+                if let Some((y, x)) = diagonal_center {
+                    diagonal_centers[y][x] += 1;
+                }
+            }
+        }
+    }
+    let num_crosses = diagonal_centers
+        .iter()
+        .map(|row| row.iter().filter(|count| **count > 1).count())
+        .sum::<usize>();
+    dbg!(num_crosses);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod day04;
 
 fn main() {
     let start = std::time::Instant::now();
-    day04::part1();
+    day04::part2();
     println!("Finished in {:?}", start.elapsed());
 }
