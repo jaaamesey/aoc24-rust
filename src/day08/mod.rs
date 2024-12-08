2c27b99242511cc5661cc08bd4da44c12fe7b6bf
James Karlsson <jameskarlsson1@gmail.com>
2024-12-08 17:38:41 +1100

    Part 2

--- src/day08/mod.rs
+++ src/day08/mod.rs
@@ -61,4 +61,53 @@ pub fn part1() {
     }
 }
 
-pub fn part2() {}
+pub fn part2() {
+    let lines = include_str!("./real_input.txt").lines();
+
+    let mut positions_by_frequency: HashMap<char, Vec<(isize, isize)>> = HashMap::new();
+
+    for (y, line) in lines.clone().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c != '.' {
+                positions_by_frequency
+                    .entry(c)
+                    .or_insert_with(Vec::new)
+                    .push((isize::try_from(y).unwrap(), isize::try_from(x).unwrap()))
+            }
+        }
+    }
+
+    let grid_height = isize::try_from(lines.clone().count()).unwrap();
+    let grid_width = isize::try_from(lines.clone().next().unwrap().chars().count()).unwrap();
+
+    let mut antinodes: HashSet<(isize, isize)> = HashSet::new();
+
+    for frequency_positions in positions_by_frequency.values() {
+        for position in frequency_positions {
+            for other_position in frequency_positions {
+                if position == other_position {
+                    continue;
+                }
+                let offset_y = other_position.0 - position.0;
+                let offset_x = other_position.1 - position.1;
+                let mut i = 0;
+                loop {
+                    let antinode_y = position.0 - i * offset_y;
+                    let antinode_x = position.1 - i * offset_x;
+                    if antinode_y >= 0
+                        && antinode_y < grid_height
+                        && antinode_x >= 0
+                        && antinode_x < grid_width
+                    {
+                        antinodes.insert((antinode_y, antinode_x));
+                    } else {
+                        break;
+                    }
+                    i += 1;
+                }
+            }
+        }
+    }
+
+    dbg!(antinodes.len());
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod day08;
 
 fn main() {
     let start = std::time::Instant::now();
-    day08::part1();
+    day08::part2();
     println!("Finished in {:?}", start.elapsed());
 }
