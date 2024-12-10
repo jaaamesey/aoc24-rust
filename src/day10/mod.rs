eed726004119f1c35e1dbb1298bebf8dd076b2c3
James Karlsson <jameskarlsson1@gmail.com>
2024-12-10 19:37:55 +1100

    Cleanup

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -72,7 +72,7 @@ pub fn part2() {
                 continue;
             }
             let mut paths_found = 0;
-            find_nines_p2(&grid, &directions, (y, x), &mut paths_found);
+            find_nines_p2(&grid, &directions, &(y, x), &mut paths_found);
             sum += paths_found;
         }
     }
@@ -82,15 +82,15 @@ pub fn part2() {
 fn find_nines_p2(
     grid: &Vec<Vec<u8>>,
     directions: &[(isize, isize)],
-    (y, x): (usize, usize),
+    (y, x): &(usize, usize),
     paths_found: &mut usize,
 ) {
     let height = grid.len();
     let width = grid[0].len();
-    let neighbours = directions.iter().filter_map(|dir| {
-        let ny = y.checked_add_signed(dir.0).unwrap_or(height);
-        let nx = x.checked_add_signed(dir.1).unwrap_or(width);
-        if ny < height && nx < width && grid[ny][nx] == grid[y][x] + 1 {
+    let neighbours = directions.iter().filter_map(|(dy, dx)| {
+        let ny = y.checked_add_signed(*dy).unwrap_or(height);
+        let nx = x.checked_add_signed(*dx).unwrap_or(width);
+        if ny < height && nx < width && grid[ny][nx] == grid[*y][*x] + 1 {
             return Some((ny, nx));
         }
         return None;
@@ -100,6 +100,6 @@ fn find_nines_p2(
             *paths_found += 1;
             continue;
         }
-        find_nines_p2(grid, directions, pos, paths_found);
+        find_nines_p2(grid, directions, &pos, paths_found);
     }
 }
