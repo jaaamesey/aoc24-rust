33a234f6d965c4901babfc252167e614f13ca94f
James Karlsson <jameskarlsson1@gmail.com>
2024-12-12 12:24:01 +1100

    Shave off another .8ms by memoizing splits

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -7,9 +7,16 @@ pub fn do_the_thing(iterations: usize) {
         .map(|s| s.parse::<usize>().unwrap())
         .for_each(|stone| *stones.entry(stone).or_insert(0) += 1);
 
+    let mut memoized_splits = HashMap::<usize, (usize, usize)>::new();
+
     for _ in 0..iterations {
         let mut new_stones: HashMap<usize, usize> = HashMap::with_capacity(stones.len() + 50);
         for (&stone_label, &stone_quantity) in stones.iter() {
+            if let Some((left, right)) = memoized_splits.get(&stone_label) {
+                *new_stones.entry(*left).or_insert(0) += stone_quantity;
+                *new_stones.entry(*right).or_insert(0) += stone_quantity;
+                continue;
+            }
             let digits = stone_label.to_string();
             if stone_label == 0 {
                 *new_stones.entry(1).or_insert(0) += stone_quantity;
@@ -17,6 +24,7 @@ pub fn do_the_thing(iterations: usize) {
                 let (left_str, right_str) = digits.split_at(digits.len() / 2);
                 let left = left_str.parse::<usize>().unwrap();
                 let right = right_str.parse::<usize>().unwrap();
+                memoized_splits.insert(stone_label, (left, right));
                 *new_stones.entry(left).or_insert(0) += stone_quantity;
                 *new_stones.entry(right).or_insert(0) += stone_quantity;
             } else {
