44f72787b8dafc4b39f8fd0e53afc881f4a98288
James Karlsson <jameskarlsson1@gmail.com>
2024-12-12 12:28:00 +1100

    Remove another millisecond by memoizing everything

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -7,28 +7,33 @@ pub fn do_the_thing(iterations: usize) {
         .map(|s| s.parse::<usize>().unwrap())
         .for_each(|stone| *stones.entry(stone).or_insert(0) += 1);
 
-    let mut memoized_splits = HashMap::<usize, (usize, usize)>::new();
+    let mut memoized = HashMap::<usize, (usize, Option<usize>)>::new();
 
     for _ in 0..iterations {
         let mut new_stones: HashMap<usize, usize> = HashMap::with_capacity(stones.len() + 50);
         for (&stone_label, &stone_quantity) in stones.iter() {
-            if let Some((left, right)) = memoized_splits.get(&stone_label) {
+            if let Some((left, right)) = memoized.get(&stone_label) {
                 *new_stones.entry(*left).or_insert(0) += stone_quantity;
-                *new_stones.entry(*right).or_insert(0) += stone_quantity;
+                if let Some(right) = right {
+                    *new_stones.entry(*right).or_insert(0) += stone_quantity;
+                }
                 continue;
             }
             let digits = stone_label.to_string();
             if stone_label == 0 {
                 *new_stones.entry(1).or_insert(0) += stone_quantity;
+                memoized.insert(stone_label, (1, None));
             } else if digits.len() % 2 == 0 {
                 let (left_str, right_str) = digits.split_at(digits.len() / 2);
                 let left = left_str.parse::<usize>().unwrap();
                 let right = right_str.parse::<usize>().unwrap();
-                memoized_splits.insert(stone_label, (left, right));
+                memoized.insert(stone_label, (left, Some(right)));
                 *new_stones.entry(left).or_insert(0) += stone_quantity;
                 *new_stones.entry(right).or_insert(0) += stone_quantity;
             } else {
-                *new_stones.entry(stone_label * 2024).or_insert(0) += stone_quantity;
+                let new_label = stone_label * 2024;
+                *new_stones.entry(new_label).or_insert(0) += stone_quantity;
+                memoized.insert(stone_label, (new_label, None));
             }
         }
         stones = new_stones;
