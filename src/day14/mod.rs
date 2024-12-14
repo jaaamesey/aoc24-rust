fed36aaacb48381c315c140b7ae1d0271f839d86
James Karlsson <jameskarlsson1@gmail.com>
2024-12-14 17:37:53 +1100

    Part 2

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -1,3 +1,5 @@
+use hashbrown::HashSet;
+
 #[derive(Debug)]
 struct Robot {
     pos: (isize, isize),
@@ -62,4 +64,39 @@ pub fn part1() {
     dbg!(quadrant_a_count * quadrant_b_count * quadrant_c_count * quadrant_d_count);
 }
 
-pub fn part2() {}
+pub fn part2() {
+    let mut lines = include_str!("./real_input.txt").lines();
+    let (height_str, width_str) = lines.next().unwrap().split_once(",").unwrap();
+    let height = height_str.parse::<isize>().unwrap();
+    let width = width_str.parse::<isize>().unwrap();
+
+    let mut robots: Vec<Robot> = lines
+        .map(|line| {
+            let (mut pos_str, mut vel_str) = line.split_once(" ").unwrap();
+            pos_str = &pos_str[2..];
+            vel_str = &vel_str[2..];
+            let (pos_x_str, pos_y_str) = pos_str.split_once(",").unwrap();
+            let (vel_x_str, vel_y_str) = vel_str.split_once(",").unwrap();
+            Robot {
+                pos: (pos_y_str.parse().unwrap(), pos_x_str.parse().unwrap()),
+                vel: (vel_y_str.parse().unwrap(), vel_x_str.parse().unwrap()),
+            }
+        })
+        .collect();
+
+    let mut i = 0;
+    loop {
+        i += 1;
+        let mut positions = HashSet::<(isize, isize)>::with_capacity(robots.len());
+        robots.iter_mut().for_each(|r| {
+            r.pos.0 = (r.pos.0 + r.vel.0).rem_euclid(height);
+            r.pos.1 = (r.pos.1 + r.vel.1).rem_euclid(width);
+            positions.insert(r.pos);
+        });
+        if positions.len() == robots.len() {
+            dbg!(i);
+            dbg!(positions.len(), robots.len());
+            break;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod day14;
 
 fn main() {
     let start = std::time::Instant::now();
-    day14::part1();
+    day14::part2();
     println!("Finished in {:?}", start.elapsed());
 }
